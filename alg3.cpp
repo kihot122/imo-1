49bd4652c43073edcf59e766d31644b6d3a328a6
Michał Kowalewski <m.kowalewski@outlook.com>
2022-03-20 20:24:17 +0000

    A3 fix

--- alg3.cpp
+++ alg3.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <array>
+#include <deque>
 #include <random>
 #include <set>
 #include "main.hpp"
@@ -17,10 +18,16 @@ std::vector<int> Alg3(std::vector<std::vector<int>> Matrix, int StartNode)
 
 	int PivotA = StartNode == -1 ? Dist(Engine) : StartNode;
 	int PivotB = std::distance(Matrix[PivotA].begin(), std::max_element(Matrix[PivotA].begin(), Matrix[PivotA].end()));
-	int SecondA = std::distance(Matrix[PivotA].begin(), std::min_element(Matrix[PivotA].begin(), Matrix[PivotA].end(), [&](auto &a, auto &b) { return a == 0 ? false : a < b; }));
-	int SecondB = std::distance(Matrix[PivotB].begin(), std::min_element(Matrix[PivotB].begin(), Matrix[PivotB].end(), [&](auto &a, auto &b) { return a == 0 ? false : a < b; }));
 
-	std::vector<int> CycleA{PivotA, SecondA}, CycleB{PivotB, SecondB};
+	for (auto &Row : Matrix)
+		for (auto &Col : Row)
+			if (Col == 0)
+				Col = 0x0fffffff;
+
+	int SecondA = std::distance(Matrix[PivotA].begin(), std::min_element(Matrix[PivotA].begin(), Matrix[PivotA].end()));
+	int SecondB = std::distance(Matrix[PivotB].begin(), std::min_element(Matrix[PivotB].begin(), Matrix[PivotB].end()));
+
+	std::deque<int> CycleA{PivotA, SecondA}, CycleB{PivotB, SecondB};
 	for (auto Index : {PivotA, SecondA, PivotB, SecondB})
 		AvailableNodes.erase(Index);
 
@@ -28,106 +35,107 @@ std::vector<int> Alg3(std::vector<std::vector<int>> Matrix, int StartNode)
 	while (not AvailableNodes.empty())
 	{
 		std::vector<std::array<int, 2>> Deltas;
-		std::vector<int> Indices;
+		std::vector<int> Nodes;
+		std::vector<int> Places;
 
 		int WorstRegret = -1;
 		int BestDelta = 0x0fffffff;
-		int SelectedIndex = -1;
+		int BestNode = -1;
+		int BestPlace = -1;
 
 		if (Flag)
 		{
 			for (auto Candidate : AvailableNodes)
 			{
 				Deltas.push_back({0x0fffffff, 0x0fffffff});
-				Indices.push_back(-1);
-				auto &Best = Deltas.back();
-				int &BestIndex = Indices.back();
+				Nodes.push_back(Candidate);
+				Places.push_back(-1);
+
+				auto &BestDeltas = Deltas.back();
+				int &BestPlace = Places.back();
 				int Delta = 0;
 
 				for (size_t i = 0; i < CycleA.size(); i++)
 				{
 					Delta = Matrix[CycleA[i]][Candidate] + Matrix[Candidate][CycleA[(i + 1) % CycleA.size()]] - Matrix[CycleA[i]][CycleA[(i + 1) % CycleA.size()]];
-					if (Delta < Best[0])
+					if (Delta < BestDeltas[0])
 					{
-						Best[1] = Best[0];
-						Best[0] = Delta;
-						BestIndex = Candidate;
+						BestDeltas[1] = BestDeltas[0];
+						BestDeltas[0] = Delta;
+						BestPlace = i;
 					}
-					else if (Delta < Best[1])
+					else if (Delta < BestDeltas[1])
 					{
-						Best[1] = Delta;
+						BestDeltas[1] = Delta;
 					}
 				}
 			}
 
-			for (size_t i = 0; i < Deltas.size(); i++)
+			for (size_t i = 0; i < Nodes.size(); i++)
 			{
 				int Regret = Deltas[i][1] - Deltas[i][0];
-				if (Regret > WorstRegret)
+				if (Regret > WorstRegret or Regret == WorstRegret and Deltas[i][0] < BestDelta)
 				{
 					WorstRegret = Regret;
-					SelectedIndex = Indices[i];
 					BestDelta = Deltas[i][0];
-				}
-				else if (Regret == WorstRegret and Deltas[i][0] < BestDelta)
-				{
-					BestDelta = Deltas[i][0];
-					SelectedIndex = Indices[i];
+					BestNode = Nodes[i];
+					BestPlace = Places[i];
 				}
 			}
 
-			CycleA.push_back(SelectedIndex);
+			CycleA.insert(CycleA.begin() + BestPlace + 1, BestNode);
 		}
 		else
 		{
 			for (auto Candidate : AvailableNodes)
 			{
 				Deltas.push_back({0x0fffffff, 0x0fffffff});
-				Indices.push_back(-1);
-				auto &Best = Deltas.back();
-				int &BestIndex = Indices.back();
+				Nodes.push_back(Candidate);
+				Places.push_back(-1);
+
+				auto &BestDeltas = Deltas.back();
+				int &BestPlace = Places.back();
 				int Delta = 0;
 
 				for (size_t i = 0; i < CycleB.size(); i++)
 				{
 					Delta = Matrix[CycleB[i]][Candidate] + Matrix[Candidate][CycleB[(i + 1) % CycleB.size()]] - Matrix[CycleB[i]][CycleB[(i + 1) % CycleB.size()]];
-					if (Delta < Best[0])
+					if (Delta < BestDeltas[0])
 					{
-						Best[1] = Best[0];
-						Best[0] = Delta;
-						BestIndex = Candidate;
+						BestDeltas[1] = BestDeltas[0];
+						BestDeltas[0] = Delta;
+						BestPlace = i;
 					}
-					else if (Delta < Best[1])
+					else if (Delta < BestDeltas[1])
 					{
-						Best[1] = Delta;
+						BestDeltas[1] = Delta;
 					}
 				}
 			}
 
-			for (size_t i = 0; i < Deltas.size(); i++)
+			for (size_t i = 0; i < Nodes.size(); i++)
 			{
 				int Regret = Deltas[i][1] - Deltas[i][0];
-				if (Regret > WorstRegret)
+				if (Regret > WorstRegret or Regret == WorstRegret and Deltas[i][0] < BestDelta)
 				{
 					WorstRegret = Regret;
-					SelectedIndex = Indices[i];
-				}
-				else if (Regret == WorstRegret and Deltas[i][0] < BestDelta)
-				{
 					BestDelta = Deltas[i][0];
-					SelectedIndex = Indices[i];
+					BestNode = Nodes[i];
+					BestPlace = Places[i];
 				}
 			}
 
-			CycleB.push_back(SelectedIndex);
+			CycleB.insert(CycleB.begin() + BestPlace + 1, BestNode);
 		}
 
 		Deltas.clear();
-		Indices.clear();
-		AvailableNodes.erase(SelectedIndex);
+		Nodes.clear();
+		Places.clear();
+
+		AvailableNodes.erase(BestNode);
 		Flag = not Flag;
 	}
 
 	CycleA.insert(CycleA.end(), CycleB.begin(), CycleB.end());
-	return CycleA;
+	return std::vector<int>(CycleA.begin(), CycleA.end());
 }
