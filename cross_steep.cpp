71c03ab5a167773b7444ce1eebc376dff649eb59
Michał Kowalewski <m.kowalewski@outlook.com>
2022-03-29 14:06:08 +0000

    stash

---
+++ cross_steep.cpp
@@ -0,0 +1,11 @@
+#include "main.hpp"
+
+std::vector<int> CrossSteep(std::vector<std::vector<int>> Matrix, std::vector<int> Cycles)
+{
+    int Delta = 0;
+    do
+    {
+        
+    } while (Delta < 0);
+    
+}
