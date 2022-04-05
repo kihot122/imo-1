39cbe9d349e9cd07fd88c6b2a9cf88f1239398bb
Michał Kowalewski <m.kowalewski@outlook.com>
2022-04-05 12:32:41 +0000

    vertex steep impl.

--- vertex_steep.cpp
+++ vertex_steep.cpp
@@ -1,16 +1,68 @@
 #include "main.hpp"
 
-std::vector<int> CrossSteep(std::vector<std::vector<int>> Matrix, std::vector<int> Cycles)
+enum class VertexType : bool
 {
-	auto CycleA = std::vector<int>(Cycles.begin(), Cycles.begin() + Cycles.size() / 2);
-	auto CycleB = std::vector<int>(Cycles.begin() + Cycles.size() / 2, Cycles.end());
+	LOCAL,
+	CROSS,
+};
+
+std::vector<int> VertexSteep(std::vector<std::vector<int>> Matrix, std::vector<int> Cycles)
+{
+	std::array CyclesArr = {std::vector<int>(Cycles.begin(), Cycles.begin() + Cycles.size() / 2), std::vector<int>(Cycles.begin() + Cycles.size() / 2, Cycles.end())};
+	auto &CycleA = CyclesArr[0];
+	auto &CycleB = CyclesArr[1];
 
 	int MinDelta;
 	do
 	{
 		MinDelta = 0;
-		int ChoiceA, ChoiceB;
 		int Delta;
+		int ChoiceA, ChoiceB, CycleIndex;
+		VertexType Type;
+
+		for (int i = 0; auto &&Cycle : CyclesArr)
+		{
+			for (int A = 0; A < Cycle.size(); A++)
+			{
+				for (int B = A + 1; B < Cycle.size(); B++)
+				{
+					int Size = static_cast<int>(Cycle.size());
+
+					int AMin = A - 1 < 0 ? Size - 1 : A - 1;
+					int BMin = B - 1 < 0 ? Size - 1 : B - 1;
+
+					int AMax = A + 1 == Size ? 0 : A + 1;
+					int BMax = B + 1 == Size ? 0 : B + 1;
+
+					//(A-1 to B) + (A+1 to B) + (B-1 to A) + (B+1 to A)
+					//- (A-1 to A) - (A+1 to A) - (B-1 to B) - (B+1 to B)
+					if (AMax != B and BMax != A)
+						Delta = Matrix[Cycle[AMin]][Cycle[B]] + Matrix[Cycle[AMax]][Cycle[B]] + Matrix[Cycle[BMin]][Cycle[A]] + Matrix[Cycle[BMax]][Cycle[A]] - Matrix[Cycle[AMin]][Cycle[A]] - Matrix[Cycle[AMax]][Cycle[A]] -
+								Matrix[Cycle[BMin]][Cycle[B]] - Matrix[Cycle[BMax]][Cycle[B]];
+
+					//(A-1 to B) + (B+1 to A)
+					//- (A-1 to A) - (B+1 to B)
+					else if (BMax != A)
+						Delta = Matrix[Cycle[AMin]][Cycle[B]] + Matrix[Cycle[BMax]][Cycle[A]] - Matrix[Cycle[AMin]][Cycle[A]] - Matrix[Cycle[BMax]][Cycle[B]];
+
+					//(A to B-1) + (B to A+1)
+					//- (A to A+1) - (B to B-1)
+					else
+						Delta = Matrix[Cycle[A]][Cycle[BMin]] + Matrix[Cycle[B]][Cycle[AMax]] - Matrix[Cycle[A]][Cycle[AMax]] - Matrix[Cycle[B]][Cycle[BMin]];
+
+					if (Delta < MinDelta)
+					{
+						MinDelta = Delta;
+						ChoiceA = A;
+						ChoiceB = B;
+						CycleIndex = i;
+						Type = VertexType::LOCAL;
+					}
+				}
+			}
+			i++;
+		}
+
 		for (int A = 0; A < CycleA.size(); A++)
 		{
 			for (int B = 0; B < CycleB.size(); B++)
@@ -34,14 +86,32 @@ std::vector<int> CrossSteep(std::vector<std::vector<int>> Matrix, std::vector<in
 					MinDelta = Delta;
 					ChoiceA = A;
 					ChoiceB = B;
+					Type = VertexType::CROSS;
 				}
 			}
 		}
+
 		if (MinDelta < 0)
-			std::swap(CycleA[ChoiceA], CycleB[ChoiceB]);
+		{
+			switch (Type)
+			{
+				case VertexType::LOCAL:
+				{
+					std::swap(CyclesArr[CycleIndex][ChoiceA], CyclesArr[CycleIndex][ChoiceB]);
+					break;
+				}
+				case VertexType::CROSS:
+				{
+					std::swap(CycleA[ChoiceA], CycleB[ChoiceB]);
+					break;
+				}
+			}
+		}
 
 	} while (MinDelta < 0);
 
-	CycleA.insert(CycleA.end(), CycleB.begin(), CycleB.end());
-	return std::vector<int>(CycleA.begin(), CycleA.end());
+	std::vector<int> Temp = CyclesArr[0];
+	Temp.insert(Temp.end(), CyclesArr[1].begin(), CyclesArr[1].end());
+
+	return Temp;
 }
