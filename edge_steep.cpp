7a26feffa873bd614ccc16c90d8a5513175d8f5a
Michał Kowalewski <m.kowalewski@outlook.com>
2022-04-05 12:47:47 +0000

    edge steep impl.

--- edge_steep.cpp
+++ edge_steep.cpp
@@ -1,27 +1,27 @@
 #include <algorithm>
 #include "main.hpp"
 
-enum class EType : bool
+enum class ExchangeType : bool
 {
-	VERTEX,
-	EDGE,
+	LOCAL,
+	CROSS,
 };
 
-std::vector<int> LocalSteep(std::vector<std::vector<int>> Matrix, std::vector<int> Cycles)
+std::vector<int> EdgeSteep(std::vector<std::vector<int>> Matrix, std::vector<int> Cycles)
 {
 	std::array CyclesArr = {std::vector<int>(Cycles.begin(), Cycles.begin() + Cycles.size() / 2), std::vector<int>(Cycles.begin() + Cycles.size() / 2, Cycles.end())};
-	int MinDelta;
-	bool Flag;
+	auto &CycleA = CyclesArr[0];
+	auto &CycleB = CyclesArr[1];
 
+	int MinDelta;
 	do
 	{
 		MinDelta = 0;
-		Flag = false;
-		int ChoiceA, ChoiceB;
 		int Delta;
-		EType Type;
+		int ChoiceA, ChoiceB, CycleIndex;
+		ExchangeType Type;
 
-		for (auto &&Cycle : CyclesArr)
+		for (int i = 0; auto &&Cycle : CyclesArr)
 		{
 			for (int A = 0; A < Cycle.size(); A++)
 			{
@@ -35,30 +35,6 @@ std::vector<int> LocalSteep(std::vector<std::vector<int>> Matrix, std::vector<in
 					int AMax = A + 1 == Size ? 0 : A + 1;
 					int BMax = B + 1 == Size ? 0 : B + 1;
 
-					//(A-1 to B) + (A+1 to B) + (B-1 to A) + (B+1 to A)
-					//- (A-1 to A) - (A+1 to A) - (B-1 to B) - (B+1 to B)
-					if (AMax != B and BMax != A)
-						Delta = Matrix[Cycle[AMin]][Cycle[B]] + Matrix[Cycle[AMax]][Cycle[B]] + Matrix[Cycle[BMin]][Cycle[A]] + Matrix[Cycle[BMax]][Cycle[A]] - Matrix[Cycle[AMin]][Cycle[A]] - Matrix[Cycle[AMax]][Cycle[A]] -
-								Matrix[Cycle[BMin]][Cycle[B]] - Matrix[Cycle[BMax]][Cycle[B]];
-
-					//(A-1 to B) + (B+1 to A)
-					//- (A-1 to A) - (B+1 to B)
-					else if (BMax != A)
-						Delta = Matrix[Cycle[AMin]][Cycle[B]] + Matrix[Cycle[BMax]][Cycle[A]] - Matrix[Cycle[AMin]][Cycle[A]] - Matrix[Cycle[BMax]][Cycle[B]];
-
-					//(A to B-1) + (B to A+1)
-					//- (A to A+1) - (B to B-1)
-					else
-						Delta = Matrix[Cycle[A]][Cycle[BMin]] + Matrix[Cycle[B]][Cycle[AMax]] - Matrix[Cycle[A]][Cycle[AMax]] - Matrix[Cycle[B]][Cycle[BMin]];
-
-					if (Delta < MinDelta)
-					{
-						MinDelta = Delta;
-						ChoiceA = A;
-						ChoiceB = B;
-						Type = EType::VERTEX;
-					}
-
 					//(A to B) + (A+1 to B+1) - (A to A+1) - (B + B+1)
 					Delta = Matrix[Cycle[A]][Cycle[B]] + Matrix[Cycle[AMax]][Cycle[BMax]] - Matrix[Cycle[A]][Cycle[AMax]] - Matrix[Cycle[B]][Cycle[BMax]];
 
@@ -67,33 +43,63 @@ std::vector<int> LocalSteep(std::vector<std::vector<int>> Matrix, std::vector<in
 						MinDelta = Delta;
 						ChoiceA = A;
 						ChoiceB = B;
-						Type = EType::EDGE;
+						CycleIndex = i;
+						Type = ExchangeType::LOCAL;
 					}
 				}
 			}
+			i++;
+		}
 
-			if (MinDelta < 0)
+		for (int A = 0; A < CycleA.size(); A++)
+		{
+			for (int B = 0; B < CycleB.size(); B++)
 			{
-				Flag = true;
-				switch (Type)
+				int SizeA = static_cast<int>(CycleA.size());
+				int SizeB = static_cast<int>(CycleB.size());
+
+				int AMin = A - 1 < 0 ? SizeA - 1 : A - 1;
+				int BMin = B - 1 < 0 ? SizeB - 1 : B - 1;
+
+				int AMax = A + 1 == SizeA ? 0 : A + 1;
+				int BMax = B + 1 == SizeB ? 0 : B + 1;
+
+				//(A-1 to B) + (A+1 to B) + (B-1 to A) + (B+1 to A)
+				//- (A-1 to A) - (A+1 to A) - (B-1 to B) - (B+1 to B)
+				Delta = Matrix[CycleA[AMin]][CycleB[B]] + Matrix[CycleA[AMax]][CycleB[B]] + Matrix[CycleB[BMin]][CycleA[A]] + Matrix[CycleB[BMax]][CycleA[A]] - Matrix[CycleA[AMin]][CycleA[A]] - Matrix[CycleA[AMax]][CycleA[A]] -
+						Matrix[CycleB[BMin]][CycleB[B]] - Matrix[CycleB[BMax]][CycleB[B]];
+
+				if (Delta < MinDelta)
 				{
-					case EType::VERTEX:
-					{
-						std::swap(Cycle[ChoiceA], Cycle[ChoiceB]);
-						break;
-					}
-					case EType::EDGE:
-					{
-						std::reverse(Cycle.begin() + ChoiceA + 1, Cycle.begin() + ChoiceB + 1);
-						break;
-					}
+					MinDelta = Delta;
+					ChoiceA = A;
+					ChoiceB = B;
+					Type = ExchangeType::CROSS;
 				}
 			}
 		}
 
-	} while (Flag);
+		if (MinDelta < 0)
+		{
+			switch (Type)
+			{
+				case ExchangeType::LOCAL:
+				{
+					std::reverse(CyclesArr[CycleIndex].begin() + ChoiceA + 1, CyclesArr[CycleIndex].begin() + ChoiceB + 1);
+					break;
+				}
+				case ExchangeType::CROSS:
+				{
+					std::swap(CycleA[ChoiceA], CycleB[ChoiceB]);
+					break;
+				}
+			}
+		}
+
+	} while (MinDelta < 0);
 
 	std::vector<int> Temp = CyclesArr[0];
 	Temp.insert(Temp.end(), CyclesArr[1].begin(), CyclesArr[1].end());
+
 	return Temp;
 }
