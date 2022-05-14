c7c478be2eec2099022b9b170efba43b53b8d309
Michał Kowalewski <m.kowalewski@outlook.com>
2022-05-14 12:45:36 +0200

    .

--- vertex_steep.cpp
+++ vertex_steep.cpp
@@ -9,7 +9,6 @@ enum class ExchangeType : bool
 
 std::vector<int> VertexSteep(std::vector<std::vector<int>> Matrix, std::vector<int> Cycles)
 {
-	std::array Test{2, 3};
 	std::array CyclesArr{std::vector<int>(Cycles.begin(), Cycles.begin() + Cycles.size() / 2), std::vector<int>(Cycles.begin() + Cycles.size() / 2, Cycles.end())};
 	auto &CycleA = CyclesArr[0];
 	auto &CycleB = CyclesArr[1];
