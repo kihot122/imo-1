ad9f9d9f2ed7bd227751ae6a69b6c6f2e8401d71
Michał Kowalewski <m.kowalewski@outlook.com>
2022-05-15 11:09:14 +0200

    refactor main header

--- main.hpp
+++ main.hpp
@@ -4,11 +4,14 @@
 #include <tuple>
 #include <vector>
 
-std::vector<std::vector<int>> ReadMat(const std::string &Filename);
+typedef std::vector<std::vector<int>> VMat;
+typedef std::vector<int> VCycle;
+
+VMat ReadMat(const std::string &Filename);
 std::vector<std::tuple<int, int>> ReadPos(const std::string &Filename);
 void WritePos(const std::vector<std::tuple<int, int>> &Vec, const std::string &Filename);
-void WriteChain(const std::vector<int> &Cycles, const std::vector<std::tuple<int, int>> &Positions, const std::string &Filename);
-int ChainLength(const std::vector<int> &Cycles, const std::vector<std::vector<int>> &Matrix);
+void WriteChain(const VCycle &Cycles, const std::vector<std::tuple<int, int>> &Positions, const std::string &Filename);
+int ChainLength(const VCycle &Cycles, const VMat &Matrix);
 
 /**
  * @brief Get the random chain, best of number of rolls
@@ -17,43 +20,45 @@ int ChainLength(const std::vector<int> &Cycles, const std::vector<std::vector<in
  * @param Tries Number of rolls
  * @return 2 consecutive cycles
  */
-std::vector<int> RandomChain(std::vector<std::vector<int>> Matrix, int Tries = 100);
+VCycle RandomChain(VMat Matrix, int Tries = 100);
 
-std::vector<int> Alg1(std::vector<std::vector<int>> Matrix, int StartNode = -1);
-std::vector<int> Alg2(std::vector<std::vector<int>> Matrix, int StartNode = -1);
-std::vector<int> Alg3(std::vector<std::vector<int>> Matrix, int StartNode = -1);
+VCycle Alg1(VMat Matrix, int StartNode = -1);
+VCycle Alg2(VMat Matrix, int StartNode = -1);
+VCycle Alg3(VMat Matrix, int StartNode = -1);
 
-std::vector<int> VertexRandom(std::vector<std::vector<int>> Matrix, std::vector<int> Cycles, uint64_t TimeNs);
-std::vector<int> VertexGreedy(std::vector<std::vector<int>> Matrix, std::vector<int> Cycles);
-std::vector<int> VertexSteep(std::vector<std::vector<int>> Matrix, std::vector<int> Cycles);
-std::vector<int> EdgeRandom(std::vector<std::vector<int>> Matrix, std::vector<int> Cycles, uint64_t TimeNs);
-std::vector<int> EdgeGreedy(std::vector<std::vector<int>> Matrix, std::vector<int> Cycles);
-std::vector<int> EdgeSteep(std::vector<std::vector<int>> Matrix, std::vector<int> Cycles);
-
-/**
- * @brief Improved algorithm upon edge exchange. Modification - best moves list.
- *
- * @param Matrix Adjacency matrix
- * @param Cycles Base solution
- * @return std::vector<int> Improved base solution
- */
-std::vector<int> EdgeSteepVar1(std::vector<std::vector<int>> Matrix, std::vector<int> Cycles);
+VCycle VertexRandom(VMat Matrix, VCycle Cycles, uint64_t TimeNs);
+VCycle VertexGreedy(VMat Matrix, VCycle Cycles);
+VCycle VertexSteep(VMat Matrix, VCycle Cycles);
+VCycle EdgeRandom(VMat Matrix, VCycle Cycles, uint64_t TimeNs);
+VCycle EdgeGreedy(VMat Matrix, VCycle Cycles);
+VCycle EdgeSteep(VMat Matrix, VCycle Cycles);
 
 /**
  * @brief Improved algorithm upon edge exchange. Modification - best moves list.
  *
  * @param Matrix Adjacency matrix
  * @param Cycles Base solution
- * @return std::vector<int> Improved base solution
+ * @return VCycle Improved base solution
  */
-std::vector<int> EdgeSteepVar2(std::vector<std::vector<int>> Matrix, std::vector<int> Cycles, int candidatesCount = 5);
+VCycle EdgeSteepVar1(VMat Matrix, VCycle Cycles);
+
+/**
+ * @brief Improved algorithm upon edge exchange. Modification - candidate moves.
+ *
+ * @param Matrix Adjacency matrix
+ * @param Cycles Base solution
+ * @return VCycle Improved base solution
+ */
+VCycle EdgeSteepVar2(VMat Matrix, VCycle Cycles, int CandidatesCount = 5);
+
+VCycle EdgeSteepIterated1(VMat Matrix, VCycle Cycles, int Tries = 100);
 
 /**
  * @brief Return adjacent indeses from cycle.
  *
  * @param Cycle the cycle
- * @param middleIndex cycle index of returned and in between of returned values
+ * @param MiddleIndex cycle index of returned and in between of returned values
  *
  * @return array of previous, pointed and next value of specified by given index
  **/
-std::array<int, 3> getFromCyclePrevMiddleNext(std::vector<int> &Cycle, int middleIndex);
+std::array<int, 3> getFromCyclePrevMiddleNext(VCycle &Cycle, int MiddleIndex);
